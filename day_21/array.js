d0eba643361d8776ac95f8ce8d2d8a3d0895ce45
shaikmohammedabdulazeez <shaikmohammedabdulazeez@gmail.com>
2025-04-21 17:45:27 +0530

    notes

---
+++ day_21/array.js
@@ -0,0 +1,27 @@
+let arr1=[10,20,30,40,50]
+
+console.log(arr1);
+
+let arr2=['hlo','world','this','is','array']
+console.log(arr2);
+console.log(arr2[0]);
+console.log(arr2[1]);
+console.log(arr1.length);//5
+//creating array using new keyword
+let arr3=new Array(10,20,30,40,50);
+console.log(arr3);
+
+let arr4=new Array(3);//[_,_,_]
+console.log(arr4);//[empty × 3]
+console.log(arr4.length);//3
+
+
+let arr5=new Array(1);
+arr5[0]=10;
+console.log(arr5);//[10]//array containing
+
+
+
+
+
+
